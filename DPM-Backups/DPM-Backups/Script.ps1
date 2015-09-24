5718a889082681473254179902d15950bf0d11aa
james Tighe <james@james-tighe.co.uk>
2015-09-24 19:25:44 +0100

    Inital Commit

---
+++ DPM-Backups/DPM-Backups/Script.ps1
@@ -0,0 +1,23 @@
+﻿#	Author: James Tighe
+#	DOM-RPToTape.ps1
+#	Date: 24/09/2015
+
+
+$libType = Read-Host "Which Library? Quantum or MSL?"
+if ($libType -match "Quantum" -or "i500") {
+	$libType = "ADIC Scalar i500 Tape Library"
+}
+elseif ($libType -match "MSL" -or "HP") {
+	$libType = "Hewlett Packard MSL G3 Series library (x64 based)"
+}
+else {
+	Write-Host "No such library"
+}
+
+$library = Get-DPMLibrary -DPMServer THMANDPM09 | ?{$_.UserFriendlyName -match $libType}
+
+Start-DPMLibraryInventory -DPMLibrary $library -DetailedInventory -whatif
+
+$PGs = Get-ProtectionGroup -DPMServer THMANDPM09
+
+
