2e36ae6d57ee4b75af7bb4ec5de0c4899267648f
james Tighe <james@james-tighe.co.uk>
2015-10-01 17:21:56 +0100

    Added library selection using switch statement

--- DPM-Backups/DPM-Backups/Script.ps1
+++ DPM-Backups/DPM-Backups/Script.ps1
@@ -3,10 +3,7 @@
 #	Date: 24/09/2015
 
 #Get current library list from file
-
 $libraries = Get-Content "C:\TEMP\Librarylist.txt"
-
-
 CLS
 Write-Host "DPM Backup Automation Script" -ForegroundColor Black -BackgroundColor White
 Write-Host "Please Select an option. . ." 
@@ -20,8 +17,52 @@ $input = Read-Host "Enter your selection . . ."
 if ($input -eq "1")
 {
     CLS
-	$DPMServer = Read-Host "Which DPM Server?"
-	#$libType = Read-Host "Which Library? Quantum or MSL or Quantum (DPM)?"
+	[array]$libraryArray = @()
+
+	$i = 1
+	foreach ($library in $libraries)
+	{
+		$tempArray = @()
+		$tempArray = "" | Select Number, LibraryName
+
+		$tempArray.Number = $i
+		$tempArray.LibraryName = $library
+
+		$libraryArray += $tempArray
+		$i++
+	}
+
+	Write-Output $libraryArray
+	Write-Host `n
+	Write-Host "Please select a library" -ForegroundColor Black -BackgroundColor White -NoNewline
+	$librarySelect = Read-Host
+
+	While (($librarySelect -gt $libraries.count -1))
+	{
+		if ($library = " ")
+		{
+			Write-Host "You have not selected a valid library" -ForegroundColor Red
+			Start-Sleep -Seconds 2
+			CLS
+			Write-Output $libraryArray
+			Write-Host `n
+			$librarySelect = Read-Host "Please select a library"
+		}
+	}
+
+	switch ($librarySelect)
+	{
+		0 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[0]}
+		1 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[1]}
+		2 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[2]}
+		3 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[3]}
+		4 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[4]}
+		5 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[5]}
+		6 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[6]}
+		7 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[7]}
+	}
+
+
     
     while (($libType = Read-Host "Which Library? Quantum or MSL or Quantum (DPM)?") -notin $libraries) 
     {
