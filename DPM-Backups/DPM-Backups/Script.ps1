bbf05e94fd5e791174f9cf61c3be8bf7a0075e8a
james Tighe <james@james-tighe.co.uk>
2015-10-02 14:27:12 +0100

    Functions changed to add library select, confimation

--- DPM-Backups/DPM-Backups/Script.ps1
+++ DPM-Backups/DPM-Backups/Script.ps1
@@ -2,8 +2,13 @@
 #	DOM-RPToTape.ps1
 #	Date: 24/09/2015
 
-#Get current library list from file
-$libraries = Get-Content "C:\TEMP\Librarylist.txt"
+#Load function script (change to script location)
+. C:\Users\james.tighe\Documents\GitHubVisualStudio\VisualStudio\DPM-Backups\DPM-Backups\functions.ps1
+
+
+#Get current library list from DPM
+#$libraries = Get-DPMLibrary -DPMServer $DPMServer 
+
 CLS
 Write-Host "DPM Backup Automation Script" -ForegroundColor Black -BackgroundColor White
 Write-Host "Please Select an option. . ." 
@@ -17,120 +22,23 @@ $input = Read-Host "Enter your selection . . ."
 if ($input -eq "1")
 {
     CLS
-	[array]$libraryArray = @()
-
-	$i = 1
-	foreach ($library in $libraries)
-	{
-		$tempArray = @()
-		$tempArray = "" | Select Number, LibraryName
-
-		$tempArray.Number = $i
-		$tempArray.LibraryName = $library
-
-		$libraryArray += $tempArray
-		$i++
-	}
-
-	Write-Output $libraryArray
-	Write-Host `n
-	Write-Host "Please select a library" -ForegroundColor Black -BackgroundColor White -NoNewline
-	$librarySelect = Read-Host
-
-	While (($librarySelect -gt $libraries.count -1))
-	{
-		if ($library = " ")
-		{
-			Write-Host "You have not selected a valid library" -ForegroundColor Red
-			Start-Sleep -Seconds 2
-			CLS
-			Write-Output $libraryArray
-			Write-Host `n
-			$librarySelect = Read-Host "Please select a library"
-		}
-	}
-
-	switch ($librarySelect)
-	{
-		0 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[0]}
-		1 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[1]}
-		2 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[2]}
-		3 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[3]}
-		4 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[4]}
-		5 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[5]}
-		6 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[6]}
-		7 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[7]}
-	}
-
-
-    
-    while (($libType = Read-Host "Which Library? Quantum or MSL or Quantum (DPM)?") -notin $libraries) 
-    {
-    Write-Host "No active library found . . "
-	Start-Sleep -Seconds 2
-	CLS
-    }
-
-	if ($libType -match "Quantum" -or "i500") {
-		#$libType = "ADIC Scalar i500 Tape Library"
-		$libType = "Dell ML6000 Tape Library"
-	}
-	elseif ($libType -match "MSL" -or "HP") {
-		$libType = "Hewlett Packard MSL G3 Series library (x64 based)"
-	}
-	elseif ($libType -match "DPM") {
-		$libType = "Dell ML6000 Tape Library"
-	}
-	else {
-		Write-Host "No such library"
-	}
-
-	$library = Get-DPMLibrary -DPMServer $DPMServer | ?{$_.UserFriendlyName -match $libType}
-
-	#Start-DPMLibraryInventory -DPMLibrary $library -DetailedInventory -whatif
+	LibrarySelection ($libraries)
+	Start-DPMLibraryInventory -DPMLibrary $library -DetailedInventory -whatif
+	Write-Host "Tape inventory started for library " $library.UserFriendlyName -ForegroundColor Black -BackgroundColor White
+}
+if  ($input -eq "3")
+{
+	Write-Host "Emergency Tape Backup" -ForegroundColor Black -BackgroundColor White
+	Write-Host "This will copy a specified recovery point onto tape"
+	Write-Host "For emergency purposes only. For normal backups use other backup option" -ForegroundColor red
+	LibrarySelection ($libraries)
 }
 
 $libType = Read-Host "Which Library? Quantum or MSL or QuantumDPM11?"
 
 $DPMServer = Read-Host "Which DPM Server?"
 
-if ($libType -match "Quantum" -or "i500") {
-	#$libType = "ADIC Scalar i500 Tape Library"
-	$libType = "Dell ML6000 Tape Library"
-}
-elseif ($libType -match "MSL" -or "HP") {
-	$libType = "Hewlett Packard MSL G3 Series library (x64 based)"
-}
-elseif ($libType -match "DPM") {
-	$libType = "Dell ML6000 Tape Library"
-}
-else {
-	Write-Host "No such library"
-}
-
 $library = Get-DPMLibrary -DPMServer $DPMServer | ?{$_.UserFriendlyName -match $libType}
 
 #Start-DPMLibraryInventory -DPMLibrary $library -DetailedInventory -whatif
 
-
-$PGs = Get-ProtectionGroup -DPMServer THMANDPM11 | ?{$_.Name -match "THHS2E12BE2X"} | Sort Name
-
-foreach ($PG in $PGs)
-{
-	$DSs = Get-DataSource -ProtectionGroup $PG | Sort Name
-	foreach ($DS in $DSs)
-	{
-		$RPs = Get-RecoveryPoint -DataSource $ds | ?{$_.DataLocation -eq "Disk" -and $_.IsIncremental -eq $false}
-		$RP = $($RPs | sort BackupTime)[0]
-		Write-Host "Backup for " $DS -ForegroundColor Yellow
-		Write-Host "on date " $rp.BackupTime -ForegroundColor Red
-		Write-Host "Triggering Backup to Tape Job"
-		Copy-DPMTapeData $RP -SourceLibrary $library -TapeLabel "$($DS.Computer + "\" + $DS.Name + " " + $RP.BackupTime)" -TapeOption 2 -TargetLibrary $library
-	}
-}
-
-
-#To get last datasource can you $DS = $DSS[$($DSS.count)-1] 
-
-
-#Need to consolidate the library commands into a function that can be called.
--- DPM-Backups/DPM-Backups/functions.ps1
+++ DPM-Backups/DPM-Backups/functions.ps1
@@ -1,50 +1,126 @@
+Function Menu 
+{
+	CLS
+	Write-Host "DPM Backup Automation Script" -ForegroundColor Black -BackgroundColor White
+	$Global:DPMServer = Read-Host "Please enter DPM Server name"
+	#need to add the PSSession script to connect to required server. All scripts should then run under Invoke-Command
+	
+	Write-Host "Please Select an option. . ." 
+	Write-Host `n
+	Write-Host "For Tape Inventory type " -NoNewline; Write-Host "1" -ForegroundColor black -backgroundcolor white
+	Write-Host "To start backup to tape job type " -NoNewline; Write-Host "2" -ForegroundColor black -backgroundcolor white
+	Write-Host "For emergency copy to tape jobs type " -NoNewline; Write-Host "3" -ForegroundColor black -backgroundcolor white
+
+	$input = Read-Host "Enter your selection . . ." 
+}
+
+
 Function LibrarySelection ($libraries)
 {
 	[array]$libraryArray = @()
 
-		$i = 1
-		foreach ($library in $libraries)
+	$i = 0
+	foreach ($library in $libraries)
+	{
+		$tempArray = @()
+		$tempArray = "" | Select Number, LibraryName
+
+		$tempArray.Number = $i
+		$tempArray.LibraryName = $library.UserFriendlyName
+
+		$libraryArray += $tempArray
+		$i++
+	}
+
+	Write-Output $libraryArray
+	Write-Host `n
+	Write-Host "Please select a library: " -ForegroundColor Red -BackgroundColor White -NoNewline
+	$librarySelect = Read-Host
+
+	While (($librarySelect -gt $libraries.count -1))
+	{
+		if ($library = " ")
 		{
-			$tempArray = @()
-			$tempArray = "" | Select Number, LibraryName
-
-			$tempArray.Number = $i
-			$tempArray.LibraryName = $library
-
-			$libraryArray += $tempArray
-			$i++
+			Write-Host "You have not selected a valid library" -ForegroundColor Red
+			Start-Sleep -Seconds 2
+			CLS
+			Write-Output $libraryArray
+			Write-Host `n
+			$librarySelect = Read-Host "Please select a library"
 		}
+	}
 
-		Write-Output $libraryArray
-		Write-Host `n
-		Write-Host "Please select a library" -ForegroundColor Red -BackgroundColor White -NoNewline
-		$librarySelect = Read-Host
-
-		While (($librarySelect -gt $libraries.count -1))
-		{
-			if ($library = " ")
-			{
-				Write-Host "You have not selected a valid library" -ForegroundColor Red
-				Start-Sleep -Seconds 2
-				CLS
-				Write-Output $libraryArray
-				Write-Host `n
-				$librarySelect = Read-Host "Please select a library"
-			}
-		}
-
-		switch ($librarySelect)
-		{
-			0 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[0]}
-			1 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[1]}
-			2 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[2]}
-			3 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[3]}
-			4 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[4]}
-			5 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[5]}
-			6 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[6]}
-			7 {$library = $(Get-DPMLibrary -DPMServerName $DPMServer)[7]}
-		}
-$Global:library = $library
+	$library = $(Get-DPMLibrary -DPMServername $Global:DPMServer)[$librarySelect]
+	Write-Host "You have selected " $libraryArray.UserFriendlyName
+	$Global:library = $library
 }
 
-librarySelection($libraries)
+Function CopytoTape
+{
+	$library = $Global:library
+	[array]$PGArray = @()
+	
+	$PGs = Get-ProtectionGroup -DPMServer $Global:DPMServer | Sort Name
+		$i = 0
+		foreach ($PG in $PGs)
+		{
+			$tempArray = @()
+			$tempArray = "" | Select Number, ProtectionGroup
+
+			$tempArray.Number = $i
+			$tempArray.ProtectionGroup = $PG.Name
+
+			$PGArray += $tempArray
+			$i++
+		}
+	Write-Output $PGArray
+	Write-Host `n
+	Write-Host "Please select a Protection Group. 'Back' to reselect. 'Home' for main menu." -ForegroundColor Red -BackgroundColor White -NoNewline
+	$PGSelect = Read-Host 
+
+	if ($PGSelect -eq "Back")
+	{
+        Write-Host "Copy to Tape Menu"		
+	} 
+	elseif ($PGSelect -eq "home" -or $PGSelect -eq "menu")
+    {
+        Write-Host "Main Menu"
+    }
+    else
+	{
+		$PG = $PGs[$PGSelect]
+	}
+
+	$DSs = Get-DataSource -ProtectionGroup $PG | Sort Name
+	foreach ($DS in $DSs)
+	{
+		$RPs = Get-RecoveryPoint -DataSource $ds | ?{$_.DataLocation -eq "Disk" -and $_.IsIncremental -eq $false}
+		$RP = $($RPs | sort BackupTime)[0]
+		Write-Host "Backup for " $DS -ForegroundColor Yellow
+		Write-Host "on date " $rp.BackupTime -ForegroundColor Red
+		Write-Host "Triggering Backup to Tape Job"
+		ConfirmSelection ("Confirm emergency backup","Initiates emergency tape backup","Cancel")
+		if ($Global:result -eq 0)
+		{
+		Write-Host "Triggering Backup to Tape Job " $($DS.Computer + "\" + $DS.Name + " " + $RP.BackupTime)
+		#Copy-DPMTapeData $RP -SourceLibrary $library -TapeLabel "$($DS.Computer + "\" + $DS.Name + " " + $RP.BackupTime)" -TapeOption 2 -TargetLibrary $library
+		}
+		else 
+		{
+			Write-Host "Cancelling . . . "
+			Start-Sleep -Seconds 3
+			CopytoTape
+		}
+	}
+}
+
+
+Function ConfirmSelection ($title,$YesText, $NoText)
+{
+	$message = "Are you sure you want to proceed?"
+	$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", $YesText
+	$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", $NoText
+	$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+	$result = $Host.UI.PromptForChoice($title, $message, $options, 0)
+	$Global:result = $result
+}
